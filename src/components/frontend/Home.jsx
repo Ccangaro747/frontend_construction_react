4e90685aa7a70efde1956c62f7040e5ffb57f34c
Camilo Cangaro <camilocangaro@gmail.com>
2024-09-12 12:20:45 -0300

    Añadido la estructura de navegación y estilos iniciales: Componente Home con SCSS
    -Implementada la barra de navegación con React Bootstrap en el componente Home
    -Añadida la sección de hero con fondo y estilo en el componente Home
    -Definidos colores globales y estilos básicos en SCSS
    -Configurados los estilos para navegación, botones y tipografía en SCSS

--- src/components/frontend/Home.jsx
+++ src/components/frontend/Home.jsx
@@ -1,20 +1,50 @@
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
 const Home = () => {
   return (
+    <>
     <header>
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <div className="container py-3">
+        <Navbar expand="lg">
+          <Navbar.Brand href="#home" className='logo'>
+            <span>UrbanEdge </span>Constructions
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home" className="nav-link">Home</Nav.Link>
+              <Nav.Link href="#link" className="nav-link">About Us</Nav.Link>
+              <Nav.Link href="#link" className="nav-link">Services</Nav.Link>
+              <Nav.Link href="#link" className="nav-link">Projects</Nav.Link>
+              <Nav.Link href="#link" className="nav-link">Blogs</Nav.Link>
+              <Nav.Link href="#link" className="nav-link">Contact Us</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
     </header>
+    <main>
+      <section className='section-1'>
+        <div className='hero d-flex align-items-center'>
+          <div className="container-fluid">
+            <div className="text-center">
+              <span>Welcome Amazing Constructions</span>
+              <h1> Crafting dreams with <br />precision and excellence.</h1>
+              <p>We excel at transforming visions into reality through outstanding craftsmanship and precise <br />attention to detail. With years of experience and a dedication to quality</p>
+              <div className="mt-4">
+                <a className="btn btn-primary">Contact Now</a>
+                <a className="btn btn-secondary ms-2">View Projects</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+    <footer>
+
+    </footer>
+    </>
   );
 };
 
