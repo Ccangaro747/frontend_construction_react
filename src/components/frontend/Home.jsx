567392ad9954ccf9f97632c07064f323559c3259
Camilo Cangaro <camilocangaro@gmail.com>
2024-10-01 13:21:23 -0300

    Terminadas las secciones "Our Services", "Why Choose Us" y "Our Projects".
    - Creada la sección "Our Services" con diseño de tarjetas y efectos hover.
    - Añadido contenido visual y textual en la sección "Why Choose Us" con estilos personalizados.
    - Diseñada la sección "Our Projects" con imágenes de proyectos y descripción.
    - Mejorada la interactividad y apariencia visual de las secciones mediante CSS.
    - Ajustados estilos globales para consistencia en todas las secciones.

--- src/components/frontend/Home.jsx
+++ src/components/frontend/Home.jsx
@@ -1,60 +1,282 @@
-import AboutImg from '../../assets/images/about-us.jpg'
-import Footer from '../common/Footer';
-import Header from '../common/Header';
+import AboutImg from "../../assets/images/about-us.jpg";
+import Footer from "../common/Footer";
+import Header from "../common/Header";
+import ServiceImg from '../../assets/images/construction1.jpg';
+import ConstructionImg from '../../assets/images/construction2.jpg';
+import Icon1 from '../../assets/images/icon-1.svg';
+import Icon2 from '../../assets/images/icon-2.svg';
+import Icon3 from '../../assets/images/icon-3.svg';
 
 const Home = () => {
   return (
     <>
-    <Header />
-    <main>
-      {/*Hero section*/}
-      <section className='section-1'>
-        <div className='hero d-flex align-items-center'>
-          <div className="container-fluid">
-            <div className="text-center">
-              <span>Welcome Amazing Constructions</span>
-              <h1> Crafting dreams with <br />precision and excellence.</h1>
-              <p>We excel at transforming visions into reality through outstanding craftsmanship and precise <br />attention to detail. With years of experience and a dedication to quality</p>
-              <div className="mt-4">
-                <a className="btn btn-primary">Contact Now</a>
-                <a className="btn btn-secondary ms-2">View Projects</a>
+      <Header />
+      <main>
+        {/*Hero section*/}
+        <section className="section-1">
+          <div className="hero d-flex align-items-center">
+            <div className="container-fluid">
+              <div className="text-center">
+                <span>Welcome Amazing Constructions</span>
+                <h1>
+                  {" "}
+                  Crafting dreams with <br />
+                  precision and excellence.
+                </h1>
+                <p>
+                  We excel at transforming visions into reality through
+                  outstanding craftsmanship and precise <br />
+                  attention to detail. With years of experience and a dedication
+                  to quality
+                </p>
+                <div className="mt-4">
+                  <a className="btn btn-primary">Contact Now</a>
+                  <a className="btn btn-secondary ms-2">View Projects</a>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-      {/*About us section*/}
-      <section className='section-2 py-5'>
-        <div className="container py-5">
-          <div className="row">
-            <div className="col-md-6">
-              <img src={AboutImg} className="w-100" />
-            </div>
-            <div className="col-md-6">
-              <span>about us</span>
-              <h2>Crafting structures that last a lifetime</h2>
-              <p>Building enduring structures requires a comprehensive approach that combines advanced materials, resilient design, routine maintenance, and sustainable practices. By drawing on hisotircal insights and utilizing modern technology.</p>
-              <p>Designing structures that stand the test of time involves a seamless blend of cutting-edge materials, durable design, ongoing upkeep, and eco-friendly practices. By combining lessons from the past with the power of modern technology.</p>
+        </section>
+        {/*About us section*/}
+        <section className="section-2 py-5">
+          <div className="container py-5">
+            <div className="row">
+              <div className="col-md-6">
+                <img src={AboutImg} className="w-100" />
+              </div>
+              <div className="col-md-6">
+                <span>about us</span>
+                <h2>Crafting structures that last a lifetime</h2>
+                <p>
+                  Building enduring structures requires a comprehensive approach
+                  that combines advanced materials, resilient design, routine
+                  maintenance, and sustainable practices. By drawing on
+                  hisotircal insights and utilizing modern technology.
+                </p>
+                <p>
+                  Designing structures that stand the test of time involves a
+                  seamless blend of cutting-edge materials, durable design,
+                  ongoing upkeep, and eco-friendly practices. By combining
+                  lessons from the past with the power of modern technology.
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      {/*Our Services*/}
+        {/*Our Services*/}
 
-      <section className='section-3 bg-light py-5'>
-        <div className='container-fluid py-5'>
-          <div className='section-header text-center'>
-            <span>Our Services</span>
-            <h2>Our construction services</h2>
-            <p>We offer a diverse array of construction services, spanning residential, commercial, and insdustrial projects.</p>
+        <section className="section-3 bg-light py-5">
+          <div className="container-fluid py-5">
+            <div className="section-header text-center">
+              <span>Our Services</span>
+              <h2>Our construction services</h2>
+              <p>
+                We offer a diverse array of construction services, spanning
+                residential, commercial, and insdustrial projects.
+              </p>
+            </div>
+            <div className="row pt-4">
+              <div className="col-md-3 col-lg-3">
+                <div className="item">
+                  <div className="service-image">
+                    <img src={ServiceImg} alt="" className="w-100" />
+                  </div>
+                  <div className="service-body">
+                    <div className="service-title">
+                      <h3>Specialy Construction</h3>
+                    </div>
+                    <div className="service-content">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vestibulum augue at ex eleifend ultricies. Praesent sagittis turpis tortor, sit amet ultrices libero pellentesque at.</p>
+                    </div>
+                    <a href="#" className="btn btn-primary">Read More</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3 col-lg-3">
+                <div className="item">
+                  <div className="service-image">
+                    <img src={ServiceImg} alt="" className="w-100" />
+                  </div>
+                  <div className="service-body">
+                    <div className="service-title">
+                      <h3>Corporate Construction</h3>
+                    </div>
+                    <div className="service-content">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vestibulum augue at ex eleifend ultricies. Praesent sagittis turpis tortor, sit amet ultrices libero pellentesque at.</p>
+                    </div>
+                    <a href="#" className="btn btn-primary">Read More</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3 col-lg-3">
+                <div className="item">
+                  <div className="service-image">
+                    <img src={ServiceImg} alt="" className="w-100" />
+                  </div>
+                  <div className="service-body">
+                    <div className="service-title">
+                      <h3>Civil Construction</h3>
+                    </div>
+                    <div className="service-content">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vestibulum augue at ex eleifend ultricies. Praesent sagittis turpis tortor, sit amet ultrices libero pellentesque at.</p>
+                    </div>
+                    <a href="#" className="btn btn-primary">Read More</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3 col-lg-3">
+                <div className="item">
+                  <div className="service-image">
+                    <img src={ServiceImg} alt="" className="w-100" />
+                  </div>
+                  <div className="service-body">
+                    <div className="service-title">
+                      <h3>Residential Construction</h3>
+                    </div>
+                    <div className="service-content">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vestibulum augue at ex eleifend ultricies. Praesent sagittis turpis tortor, sit amet ultrices libero pellentesque at.</p>
+                    </div>
+                    <a href="#" className="btn btn-primary">Read More</a>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+
+        {/* Why Choose Us */}
+
+        <section className="section-4 py-5">
+          <div className="container py-5">
+            <div className="section-header text-center">
+              <span>Why Choose Us</span>
+              <h2>Discover our wide variety of projects</h2>
+              <p>
+                Created in close partnership with our clients and collaborators, this approach marges industry expertise, <br/>
+                decades of experience, innovation, and flexibility to consistently deliver excellence.
+              </p>
+            </div>
+            <div className="row pt-4">
+              <div className="col-md-4">
+                <div className="card shadow border-0 p-4">
+                  <div className="card-icon">
+                    <img src={Icon1} alt="" />
+                  </div>
+                  <div className="card-title mt-3">
+                    <h3>Cutting-Edge Solutions</h3>
+                  </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vestibulum augue at ex eleifend ultricies. Praesent sagittis turpis tortor, sit amet ultrices libero pellentesque at.</p>
+                </div>
+              </div>
+              <div className="col-md-4">
+                <div className="card shadow border-0 p-4">
+                  <div className="card-icon">
+                    <img src={Icon2} alt="" />
+                  </div>
+                  <div className="card-title mt-3">
+                    <h3>Superior Craftsmanship</h3>
+                  </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vestibulum augue at ex eleifend ultricies. Praesent sagittis turpis tortor, sit amet ultrices libero pellentesque at.</p>
+                </div>
+              </div>
+              <div className="col-md-4">
+                <div className="card shadow border-0 p-4">
+                  <div className="card-icon">
+                    <img src={Icon3} alt="" />
+                  </div>
+                  <div className="card-title mt-3">
+                    <h3>Knowledge and Expertise</h3>
+                  </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vestibulum augue at ex eleifend ultricies. Praesent sagittis turpis tortor, sit amet ultrices libero pellentesque at..</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
 
 
-    </main>
-    <Footer />
+        {/*Our Projects*/}
+        <section className="section-3 bg-light py-5">
+          <div className="container-fluid py-5">
+            <div className="section-header text-center">
+              <span>Our Projects</span>
+              <h2>Discover our diverse range of projects</h2>
+              <p>
+                We offer a diverse array of construction services, spanning
+                residential, commercial, and insdustrial projects.
+              </p>
+            </div>
+            <div className="row pt-4">
+              <div className="col-md-3 col-lg-3">
+                <div className="item">
+                  <div className="service-image">
+                    <img src={ConstructionImg} alt="" className="w-100" />
+                  </div>
+                  <div className="service-body">
+                    <div className="service-title">
+                      <h3>Kolkata Project</h3>
+                    </div>
+                    <div className="service-content">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vestibulum augue at ex eleifend ultricies. Praesent sagittis turpis tortor, sit amet ultrices libero pellentesque at.</p>
+                    </div>
+                    <a href="#" className="btn btn-primary">Read More</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3 col-lg-3">
+                <div className="item">
+                  <div className="service-image">
+                    <img src={ConstructionImg} alt="" className="w-100" />
+                  </div>
+                  <div className="service-body">
+                    <div className="service-title">
+                      <h3>New Delhi Project</h3>
+                    </div>
+                    <div className="service-content">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vestibulum augue at ex eleifend ultricies. Praesent sagittis turpis tortor, sit amet ultrices libero pellentesque at.</p>
+                    </div>
+                    <a href="#" className="btn btn-primary">Read More</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3 col-lg-3">
+                <div className="item">
+                  <div className="service-image">
+                    <img src={ConstructionImg} alt="" className="w-100" />
+                  </div>
+                  <div className="service-body">
+                    <div className="service-title">
+                      <h3>Kanpur Project</h3>
+                    </div>
+                    <div className="service-content">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vestibulum augue at ex eleifend ultricies. Praesent sagittis turpis tortor, sit amet ultrices libero pellentesque at.</p>
+                    </div>
+                    <a href="#" className="btn btn-primary">Read More</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3 col-lg-3">
+                <div className="item">
+                  <div className="service-image">
+                    <img src={ConstructionImg} alt="" className="w-100" />
+                  </div>
+                  <div className="service-body">
+                    <div className="service-title">
+                      <h3>Lucknow Project</h3>
+                    </div>
+                    <div className="service-content">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vestibulum augue at ex eleifend ultricies. Praesent sagittis turpis tortor, sit amet ultrices libero pellentesque at.</p>
+                    </div>
+                    <a href="#" className="btn btn-primary">Read More</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      <Footer />
     </>
   );
 };
